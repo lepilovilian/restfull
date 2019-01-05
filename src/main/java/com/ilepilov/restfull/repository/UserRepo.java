3b76826fcc14700ad62a3e467919ceb71f02402d
Ilian Lepilov <ilepilov@urchinsys.com>
2019-01-05 08:55:20 +0200

    the same with pom.xml

--- src/main/java/com/ilepilov/restfull/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ilepilov.restfull.repository;
-
-import com.ilepilov.restfull.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends CrudRepository<UserEntity, Long> {
-}
--- src/main/java/com/ilepilov/restfull/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ilepilov.restfull.service;
-
-import com.ilepilov.restfull.dto.UserDto;
-
-public interface UserService {
-    UserDto save(UserDto userDto);
-}
